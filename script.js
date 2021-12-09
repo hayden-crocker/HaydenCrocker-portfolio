d48e978b134d8591204d1205ca6c81bc64bd797d
HaydenCrocker <crocker.hayden@gmail.com>
2021-12-09 09:37:54 -0500

    remvoed JS and added CSS hover

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const projects = document.querySelectorAll('.summary')
-// const allProjects = document.querySelectorAll('.recentProjects')
+// const projects = document.querySelectorAll('.summary')
+// // const allProjects = document.querySelectorAll('.recentProjects')
 
-projects.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        // console.log(e.target.style)
-        for (element in e.target.children) {
-            e.target.style.zIndex = "10"
-        }
-        if (e.target.classList.contains('show')) {
-            e.target.classList.remove("show")
-        } else {
-            e.target.classList.add('show')
-        }
-    })
-})
+// projects.forEach((item) => {
+//     item.addEventListener('click', (e) => {
+//         // console.log(e.target.style)
+//         for (element in e.target.children) {
+//             e.target.style.zIndex = "10"
+//         }
+//         if (e.target.classList.contains('show')) {
+//             e.target.classList.remove("show")
+//         } else {
+//             e.target.classList.add('show')
+//         }
+//     })
+// })
