03c701d8677099828c5d1c3d214933c5f5f0714b
HaydenCrocker <crocker.hayden@gmail.com>
2021-12-09 00:29:33 -0500

    debugging js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const projects = document.querySelectorAll('.summary')
-const allProjects = document.querySelectorAll('.recentProjects')
+// const allProjects = document.querySelectorAll('.recentProjects')
 
 projects.forEach((item) => {
     item.addEventListener('click', (e) => {
         // console.log(e.target.style)
-        // e.target.style.display = "flex"
-        e.target.style.backgroundColor = "rgba(0, 0, 0, 1)"
         for (element in e.target.children) {
             e.target.style.zIndex = "10"
         }
-        // if (e.target.style.zIndex === "-10") {
-        //     e.target.style.zIndex = "10"
-        // } else if (e.target.style.zIndex === "10") {
-        //     e.target.style.zIndex = "-10"
-        // }
+        if (e.target.classList.contains('show')) {
+            e.target.classList.remove("show")
+        } else {
+            e.target.classList.add('show')
+        }
     })
 })
